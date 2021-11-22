eaf343ad4fad0f7999ef1a265619d86308b49a69
GazetaGaveta <godoyjmb@gmail.com>
2021-11-22 12:14:11 -0300

    22/11

--- src/addEstagiarioRoutes.js
+++ src/addEstagiarioRoutes.js
@@ -5,8 +5,11 @@ const Mongoose = require("mongoose");
 require("../src/models/Estagiario")
 const Estagiario = Mongoose.model("estagiarios");
 
+var estagiarioDigitado = {};
+
+
 Router.get('/cadastro', (req, res) => {
-    res.render('cadastroEstagiario');
+    res.render('cadastroEstagiario', estagiarioDigitado);
     //res.send("Página de cadastro de estagiário");
 });
 
@@ -18,6 +21,29 @@ Router.get('/listar', (req, res) => {
 });
 
 Router.post('/cadastro/novo', (req, res) => {
+
+    estagiarioDigitado = {
+        nome: req.body.nome,
+        dataNascimento: req.body.dataNascimento,
+        registroAcademico: req.body.registroAcademico,
+        curso: req.body.curso,
+        turno: req.body.turno,
+        cedulaDeIdentidade: req.body.cedulaDeIdentidade,
+        cpf: req.body.cpf,
+        nacionalidade: req.body.nacionalidade,
+        estadoCivil: req.body.estadoCivil,
+        cidade: req.body.cidade,
+        estado: req.body.estado,
+        cep: req.body.cep,
+        fone: req.body.fone,
+        email: req.body.email,
+    };
+    
+    if(!validarNome(req.body.nome)){
+        res.redirect("/estagiarios/cadastro");
+        return;
+    }
+    
     const novoEstagiario = {
         nome: req.body.nome,
         dataNascimento: req.body.dataNascimento,
@@ -39,9 +65,14 @@ Router.post('/cadastro/novo', (req, res) => {
     try{
         save.save();
         res.redirect("/estagiarios/listar");
+        estagiarioDigitado = {};
     }catch(err){
         console.log("falhou a criação da categoria: " + err);
     }
 });
+
+function validarNome(nome){
+    return !(!nome || typeof nome || nome == null);
+};
 
 module.exports = Router;
